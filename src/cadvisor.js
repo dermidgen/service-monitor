782284bd01d03c357d0db06c40cc45fe70e8633f
Danny Graham <good.midget@gmail.com>
2011-09-08 01:13:30 +0000

    added ook and a basic shell class for our advisor service

--- src/cadvisor.js
+++ src/cadvisor.js
@@ -3,13 +3,35 @@
  * monitoring agent.
  */
  
-//var ook = require('ook.js');
-
+require('./ook');
 var http = require('http');
 
-http.createServer(function (req, res) {
-    console.log(req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('ok\n');
-}).listen(process.env.C9_PORT, "0.0.0.0");
-console.log('Server running at http://service-monitor.dermidgen.cloud9ide.com');
+var advisor = ook.Class();
+advisor.prototype._construct = function()
+{
+  console.log('Server Starting');
+  
+  var server = function()
+  {
+      http.createServer(function (req, res) {
+        console.log(req.url);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('ok\n');
+    }).listen(process.env.C9_PORT, "0.0.0.0");
+    console.log('Server running at http://service-monitor.dermidgen.cloud9ide.com');
+  };
+  
+  server();
+};
+advisor.instance = null;
+advisor.getInstance = function()
+{
+  if (!advisor.instance) advisor.instance = new advisor();
+  return advisor.instance;
+};
+advisor.main = function()
+{
+    var app = advisor.getInstance();
+    
+};
+
